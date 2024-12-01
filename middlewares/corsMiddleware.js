bfa6ad4a09b4b06b2c718c6c049d0593c3cb400e
Zamy03 <ukasyzzam@gmail.com>
2024-12-01 12:41:55 +0700

    redeploy

---
+++ middlewares/corsMiddleware.js
@@ -0,0 +1,23 @@
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const configureMiddleware = (app) => {
+  app.use(
+    cors({
+      origin: [
+        "https://backend-eight-phi-75.vercel.app",
+        "http://localhost:3000",
+        "https://proyek-3-proyek.github.io",
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+      optionsSuccessStatus: 200,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+  app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+};
+
+export default configureMiddleware;
