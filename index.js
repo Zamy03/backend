20ad1b0c4cc60514503c7e1ed6d891140bfd3b0f
Zamy03 <ukasyzzam@gmail.com>
2024-12-02 05:17:46 +0700

    feat: add payment routes to the main application

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const kategoriRoutes = require('./routes/kategoriRoutes');
 const produkRoutes = require('./routes/produkRoutes');
+const paymentRoutes = require('./routes/paymentRoutes');
 const configureMiddleware = require('./middlewares/corsMiddleware');
 require('dotenv').config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/kategori', kategoriRoutes);
 app.use('/api/produk', produkRoutes);
+app.use('/api/payment', paymentRoutes);
 
 // Start server
 const port = process.env.PORT || 3000;
