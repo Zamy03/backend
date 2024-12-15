4e1337b77066ef0fd9a5fc1b165e7a632ca00c70
Zamy03 <ukasyzzam@gmail.com>
2024-12-15 19:48:19 +0700

    feat: update Midtrans API endpoint to use sandbox environment for transaction creation

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -81,7 +81,7 @@ const createTransaction = async (req, res) => {
             }
         };
 
-        const response = await fetch('https://app.midtrans.com/snap/v1/transactions', {
+        const response = await fetch('https://app.sandbox.midtrans.com/snap/v1/transactions', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
